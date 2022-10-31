aa8502aa1ba1ae2e06f4bf65313d90225b3effb3
Alex Juarez <112568621+thealexjuarez@users.noreply.github.com>
2022-10-31 11:07:27 -0500

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "standard",
+    "prettier",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["testing-library", "use-encapsulation", "jest-dom", "react"],
+  rules: {},
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
